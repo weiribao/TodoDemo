17951c15fd84f3aef93af5fe354ead6d668f1026
Ribao Wei <ribao.w@gmail.com>
2012-09-09 18:35:04 -0400

    edit todo

--- todo.js
+++ todo.js
@@ -2,14 +2,35 @@ var socket = io.connect();
 
 function TodoCtrl($scope){
 	$scope.todos = [];
+	$scope.editMode = false;
+	$scope.editTodo = {};
+
 	getTodos();
 
 	$scope.addTodo = function(){
+		console.log($scope.todoDue);
 		var todo = new Todo($scope.todoText, $scope.todoDue);
 		$scope.todos.push(todo);
 		$scope.todoText = "";
 		saveTodo(todo);
 	};
+
+	$scope.edit = function(todo){
+		console.log("edit");
+		$scope.editTodo = todo;
+		$scope.editMode = true;
+		$scope.todoTextEdit = todo.text;
+		$scope.todoDueEdit = numberToTime(todo.due);
+	}
+
+	$scope.saveEditTodo = function(){
+		console.log("@@@", $scope.todoTextEdit);
+		$scope.editTodo.text = $scope.todoTextEdit;
+		$scope.editTodo.due = timeToNumber($scope.todoDueEdit);
+		saveTodo($scope.editTodo);
+		$scope.editMode = false;
+		$scope.editTodo = {};
+	}
 
 	$scope.pending = function(){
 		return $scope.todos.filter(function(todo){
@@ -74,7 +95,15 @@ function Todo(text, due){
 	this._id = GUID();
 	this.text = text;
 	this.done = false;
-	this.due = Date.parse(due)+(new Date).getTimezoneOffset()*60*1000;
+	this.due = timeToNumber(due);
+}
+
+function timeToNumber(time){
+	return Date.parse(time)+(new Date).getTimezoneOffset()*60*1000
+}
+
+function numberToTime(number){
+	return moment(number).format("YYYY-MM-DD");
 }
 
 function GUID(){
